93540fde05c3f6abc27a814579b2ba92f213a819
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2023-12-22 14:04:50 -0500

    enlaces de tablas

--- database/migrations/2023_12_22_184814_create_estudiantes_table.php
+++ database/migrations/2023_12_22_184814_create_estudiantes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('estudiantes', function (Blueprint $table) {
             $table->id();
+            $table->string("nombre");
+            $table->string("apellido");
+            $table->text("foto");
             $table->timestamps();
         });
     }
